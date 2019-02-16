4f58e4c9a852c86733f50299c98c9e7eb767387e
Alex Burzinski <aburzinski2@gmail.com>
2019-02-16 17:43:03 -0600

    Attempted Question 2

--- 2-15-2019/Classic/Classic.py
+++ 2-15-2019/Classic/Classic.py
@@ -5,27 +5,41 @@ def newCarPrice():
     carPrice = [1 + random.randrange(i + 2) for i in range(5)]
     return carPrice
 
-def guessPrice(carPrice, guess, openGuesses):
-    guessedCorrectly = [True if carPrice[i] == guess[i] and openGuesses[i]
+def guessPrice(carPrice, guess, openGuesses, firstNumWait = -1):
+    if firstNumWait < 0:
+        pass
+    elif openGuesses.count(False) < firstNumWait:
+        guess[0] = 0
+    else:
+        guess[0] = carPrice[0]
+
+    # print(guess)
+    return [True if carPrice[i] == guess[i] and openGuesses[i]
         else False for i in range(5)]
-    return guessedCorrectly
 
 def updateOpenGuesses(guessedCorrectly, openGuesses):
     return [False if guessedCorrectly[i] else openGuesses[i] for i in range(5)]
 
-def updateGuess(guess, guessedCorrectly, openGuesses):
+def updateGuess(guess, guessedCorrectly, openGuesses, firstWaitNum = -1):
+    if firstWaitNum < 0:
+        pass
+    elif openGuesses.count(False) < firstWaitNum:
+        guess[0] = 0
     return [guess[i] + 1 if openGuesses[i] and not guessedCorrectly[i]
         else guess[i] for i in range(5)]
 
-def playGame():
+def playGame(firstNumWait = -1):
     carPrice = newCarPrice()
+    # print(carPrice)
     guess = [1 for _ in range(5)]
     openGuesses = [True for _ in range(5)]
     guesses = 0
 
     while True:
         guesses += 1
-        guessedCorrectly = guessPrice(carPrice, guess, openGuesses)
+        # print(openGuesses, guess)
+        guessedCorrectly = guessPrice(carPrice, guess, openGuesses, firstNumWait)
+        # print(guessedCorrectly)
         openGuesses = updateOpenGuesses(guessedCorrectly, openGuesses)
 
         if openGuesses == [False for _ in range(5)]:
@@ -33,20 +47,23 @@ def playGame():
         if guessedCorrectly == [False for _ in range(5)]:
             return (False, guesses)
 
-        guess = updateGuess(guess, guessedCorrectly, openGuesses)
+        guess = updateGuess(guess, guessedCorrectly, openGuesses, firstNumWait)
 
-def simulateGames(numGames):
+def simulateGames(numGames, firstWaitNum):
     wins = 0
     for _ in range(numGames):
-        result, totalGuesses = playGame()
+        result, _ = playGame(firstWaitNum)
         if result:
             wins += 1
     return 100.0 * wins / numGames
 
 
-numSims = 1000
+numSims = 100
 numGames = 1000
-sims = [simulateGames(numGames) for _ in range(numSims)]
 
-print('You won ' + str(sum(sims) / len(sims)) + ' percent of the time')
-print('The simulations had a standard deviation of ' + str(statistics.stdev(sims)))
+for i in range(-1, 5):
+    sims = [simulateGames(numGames, i) for _ in range(numSims)]
+    print('You won ' + str(sum(sims) / len(sims)) + ' percent of the time'
+        + ' if you wait until there are at least ' + str(i) + ' correct guesses'
+        + ' before guessing the first guess correctly')
+    print('The simulations had a standard deviation of ' + str(statistics.stdev(sims)))
